b93898a39c96d1d9a01fbef0aefe7830ee3947ba
gnanasurya <gnanasurya2@gmail.com>
2020-07-24 15:23:20 +0530

    Updates

---
+++ precache-manifest.ef85b7b13ce58b6ebeb97b18cec0853c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99ed1f3c2a2e0ee180d74a94cdd6792",
+    "url": "/car_vis/index.html"
+  },
+  {
+    "revision": "1eda24ffa0634d8f850b",
+    "url": "/car_vis/static/css/main.39845f0f.chunk.css"
+  },
+  {
+    "revision": "3b59e156c6f7dd28d2e6",
+    "url": "/car_vis/static/js/2.0a6911a9.chunk.js"
+  },
+  {
+    "revision": "6e900953f82e75bb7ccced89e6806918",
+    "url": "/car_vis/static/js/2.0a6911a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eda24ffa0634d8f850b",
+    "url": "/car_vis/static/js/main.6c601d65.chunk.js"
+  },
+  {
+    "revision": "2b3e3fc5dc583cd6aca0",
+    "url": "/car_vis/static/js/runtime-main.2c707500.js"
+  }
+]);
